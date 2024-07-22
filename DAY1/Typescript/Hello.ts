186fc5550f7597eb184ae32dcbd1e55aba1fde7b
Devesh Bhogre <devesh.bhogre@ideas.com>
2024-07-22 11:37:23 +0530

    notes push

--- DAY1/Typescript/Hello.ts
+++ DAY1/Typescript/Hello.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+var s = "hell0"; //type inferencing
+console.log(s);
+
+var a: string = "apple"; //type annotation
+
+console.log(a);
+
+//function
+function Add(x: number, y: number): number {
+    return x + y;
+}
